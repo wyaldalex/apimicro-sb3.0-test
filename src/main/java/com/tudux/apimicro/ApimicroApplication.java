45bef75126cb678a1ea6e10a737750b003eb7496
AlexAR <moltron_nano@hotmail.com>
2023-02-08 16:25:07 -0600

    feat: initial spring boot 3.0 test setup with pg

---
+++ src/main/java/com/tudux/apimicro/ApimicroApplication.java
@@ -0,0 +1,13 @@
+package com.tudux.apimicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimicroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tudux/apimicro/ApimicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tudux.apimicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
